58c277166b93507fa55d0d4811a8b040414b3051
gabriel00018 <gabrielbelinelo9@gmail.com>
2025-08-22 17:15:54 -0300

    terminamos
    terminamos dia 22/08 com o igor (ignora o nome HENRIQUE na pasta)

--- projeto_henrique/assts/js/app.js
+++ projeto_henrique/assts/js/app.js
@@ -1,35 +1,116 @@
-listaTarefas = []
-console.log(listaTarefas.length)
+var listaTarefas = []
+var listaConcluidas = []
 
-//calcula o valor de tarefas cadastradas
-document.getElementById('numTarefas').innerHTML = listaTarefas.length
-
-
-//se não tiver nenhum item na lista tem que remover a section com o ID semTarefas
+function carregaInformacoes() {
+    // Calcula o valor de tarefas cadastradas
+    document.getElementById("numTarefas").innerHTML = listaTarefas.length
 
 
-// captura o formulario de cadastro
-var formCadastro = document.getElementById('formCadastro')
 
-//adiciona umaescuta de evento no elemeento
-//Do tipo submit, quando acontecer executa todo o código da
-//function que esta no segundo parametro
-formCadastro.addEventListener("submit", function(event){
-    
-    event.preventDefault() //evita o comportamento padrão do submit. bloqueia os eventos padrões
+    document.getElementById("numConcluidas").innerHTML = listaConcluidas.length
 
-    //capturar todos os inputs do formulario
+
+    /**
+     * - EXIBIR QUANTAS TAREFAS CONCLUIDAS TEM NO SITE
+     * - MODIFICAR O IF, PARA VER SE TEM TAREFA CONCLUIDA
+     */
+
+    // Se não tiver nenhum item na lista tem que 
+    // remover a section com o ID semTarefas
+    if (listaTarefas.length === 0  &&  listaConcluidas.length ===0) {
+        document.getElementById("semTarefas").style.display = "flex"
+        document.getElementById("listaTarefas").style.display = "none"
+    } else {
+        document.getElementById("semTarefas").style.display = "none"
+        document.getElementById("listaTarefas").style.display = "flex"
+    }
+}
+
+function listaTodasTarefas() {
+
+    // Caputra a section onde irá listar as tarefas
+    var htmlListaTarefas = document.getElementById("listaTarefas")
+    htmlListaTarefas.innerHTML = "" // Limpa a section
+
+    // Percorre todas as tarefas cadastradas
+    listaTarefas.forEach(function(item, index) {
+        htmlListaTarefas.innerHTML += `
+            <div>
+                <button onclick="concluirTarefa(${index})" title="Concluir Tarefa"></button>
+                <p>${item}</p>
+                <button onclick="excluirTarefa(${index})" title="Excluir Tarefa">
+                    <img src="assts/img/lixeira.png" 
+                         alt="Icone Lixeira" />
+                </button>
+            </div>`
+
+    })
+
+
+
+    listaConcluidas.forEach(function(item, index) {
+        htmlListaTarefas.innerHTML += `
+            <div class="done">
+                <button onclick="concluirTarefa(${index})" title="Concluir Tarefa"></button>
+                <p>${item}</p>
+                <button onclick="excluirTarefa(${index})" title="Excluir Tarefa">
+                    <img src="assts/img/lixeira.png" 
+                         alt="Icone Lixeira" />
+                </button>
+            </div>`
+
+    })
+
+    /**
+     * - LISTAR AS TAREFAS JÁ CONCLUIDAS
+     */
+}
+
+function concluirTarefa(index) {
+        var item = listaTarefas.splice(index, 1) 
+        listaConcluidas.push(item)
+        carregaInformacoes()
+        listaTodasTarefas()
+    /*
+        - Deletar a tarefa do array listaTarefa
+        - Adicionar a tarefa no array listaConcluidas
+        - Atualizar as informaçoes na tela
+    */
+}
+
+
+function excluirTarefa(index) {
+    listaTarefas.splice(index, 1)
+
+    carregaInformacoes()
+    listaTodasTarefas()
+} 
+
+// Invoca
+carregaInformacoes()
+listaTodasTarefas()
+
+
+
+// Caputura o formulário de cadastro
+var formCadastro = document.getElementById("formCadastroTarefa")
+
+// Adiciona uma escuta de evento no elemento
+// Do tipo submit, quando acontecer executa todo o código da 
+// function que está no segundo parametro.
+formCadastro.addEventListener("submit", function(calopsita){
+    calopsita.preventDefault(); // Bloqueia os eventos padrões
+
+    // Caputurar todos os INPUTs do Formulario
     var form = new FormData(this);
 
-    var tarefa = form.get('tarefa')
-    console.log(tarefa)
+    // Captura o valor apenas do INPUT que possui o name tarefa
+    var tarefa = form.get("tarefa")
 
-    //salvar a tarefa na lista
-    listaTarefas.push(listaTarefas)
-    console.log(listaTarefas)
+    // Salvar a tarefa na lista
+    listaTarefas.push(tarefa)
 
-    //exibir a quantidade de tarefas no span
-    document.getElementById('numTarefas').innerHTML = listaTarefas.length
-
+    // Invoca de novo
+    carregaInformacoes()
+    listaTodasTarefas()
 })
-
